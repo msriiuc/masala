b069636fec8f2308d4bb3036b9a0a01e15ad4f38
msriiuc <69592430+msriiuc@users.noreply.github.com>
2020-09-23 01:09:23 +0600

    Add files via upload

---
+++ webscrap.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+page = requests.get('http://www.theartgardenrohingya.com/')
+
+bSoup = BeautifulSoup(page.content, 'html.parser')
+linkList = bSoup.find_all('a')
+
+for link in linkList:
+    if 'href' in link.attrs:
+        file1 = open("linkDocs.txt","w") 
+        l= (str(link.attrs['href']+ "\n"))
+        print(l)
+        
+        
+file1.writelines(l)
+file1.close()
+        #print(str(link.attrs['href']+ "\n"))
+        
+
